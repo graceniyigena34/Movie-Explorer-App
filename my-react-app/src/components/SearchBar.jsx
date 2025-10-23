0448ddc928a85285d489b0aee36aa3cd0d4cf1fc
graceniyigena34 <graceniyigena34@gmail.com>
2025-10-23 14:48:26 +0200

    made some changes in search bar

--- my-react-app/src/components/SearchBar.jsx
+++ my-react-app/src/components/SearchBar.jsx
@@ -1,11 +1,28 @@
 export default function SearchBar({ value, onChange }) {
   return (
     <input
-      type="text"
-      value={value}
-      onChange={(e) => onChange(e.target.value)}
-      placeholder="Search movies..."
-      className="w-full p-2 mb-4 rounded-lg bg-slate-700 text-white focus:ring-2 focus:ring-accent outline-none"
-    />
+  type="text"
+  value={value}
+  onChange={(e) => onChange(e.target.value)}
+  placeholder="Search movies..."
+  className="
+    w-full 
+    p-3 
+    mb-4 
+    rounded-xl 
+    bg-slate-800 
+    text-white 
+    placeholder-gray-400 
+    shadow-md 
+    focus:outline-none 
+    focus:ring-2 
+    focus:ring-accent 
+    transition 
+    duration-300 
+    ease-in-out
+    hover:bg-slate-700
+  "
+/>
+
   );
 }
