c0ac597fde6f1fb75266f1aafc02891f1fb34dbc
graceniyigena34 <graceniyigena34@gmail.com>
2025-10-23 12:19:29 +0200

    I styled the navbar

--- my-react-app/src/components/Navbar.jsx
+++ my-react-app/src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav className="sticky top-0 bg-slate-800 p-4 flex justify-center gap-6 text-lg font-semibold">
-      <Link to="/" className="hover:text-accent">Home</Link>
-      <Link to="/favorites" className="hover:text-accent">Favorites</Link>
+      <Link to="/"   className="text-white hover:text-accent transition-colors duration-300 font-medium">Home</Link>
+      <Link to="/favorites"  className="text-white hover:text-accent transition-colors duration-300 font-medium">Favorites</Link>
     </nav>
   );
 }
